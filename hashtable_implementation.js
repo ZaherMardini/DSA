50ebb87ebccdbb83bf9c391ed48c717ddba66d21
zozo <zozo@zozo.com>
2025-10-03 14:12:40 +0300

    final version of the implementation, code tours isn't working

--- hashtable_implementation.js
+++ hashtable_implementation.js
@@ -29,11 +29,39 @@ class hshtble{
     }
     return "none";
   }
+  keys(){
+    let origin = this.d;
+    let keys = [];
+    let j = 0;
+    for (let i = 0; i < origin.length; i++) {
+      j = 0;
+      while (origin[i] && origin[i][j]) {//d => [address => [bucket => [keyValue => []], bucket => [keyValue => []]], address => [bucket => [keyValue => []], bucket => [keyValue => []]]]
+        keys.push(origin[i][j][0]);
+        j++;
+      }
+    }
+    for (const key of keys) {
+      console.log(key);
+    }
+  }
+  view(){
+    for (const element of this.d) {
+      console.log(element);
+    }
+  }
 }
 
-let test = new hshtble(5);
+let test = new hshtble(2);
 test.set('kiki',500);
 test.set('momo',50);
 test.set('llll',5);
-console.log(test.get('llll'));
+test.set('testy',9);
+test.set('zefron',1);
+test.set('hornet',89);
+test.set('zote',9000);
+test.keys();
+test.view();
+console.log(test.get('zefron'));
+console.log(test.get('hornet'));
+console.log(test.get('zote'));
 console.log(test.get('sos'));
