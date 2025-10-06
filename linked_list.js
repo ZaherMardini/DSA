cabd6c0cafdd88c42762159b3b17cd0daab02d64
zozo <zozo@zozo.com>
2025-10-06 19:55:33 +0300

    finished the dobly linked list prototype

--- linked_list.js
+++ linked_list.js
@@ -2,6 +2,7 @@ class Node{
   constructor(value){
     this.value = value;
     this.next = null;
+    this.prev = null;
   }
 }
 
@@ -18,20 +19,34 @@ class linkedList{
     let element = this.head;
     let result = `head: `;
     while (element !== null) {
-      result += `${element.value} => `;
+      result += `${element.value} <=> `;
       element = element.next;
     }
     console.log(`${result}tail: ${this.tail.value}`);
     console.log(`Length: ${this.length}`);
   }
+
+  showReverse(){
+    let element = this.tail;
+    let result = `tail: `;
+    while (element !== null) {
+      result += `${element.value} <=> `;
+      element = element.prev;
+    }
+    console.log(`${result}head: ${this.head.value}`);
+    console.log(`Length: ${this.length}`);
+  }
+
   prepend(value){
     let node = new Node(value);
     node.next = this.head;
+    this.head.prev = node;
     this.head = node;
     this.length++;
   }
   append(value){
     let node = new Node(value);
+    node.prev = this.tail;
     this.tail.next = node;
     this.tail = node;
     this.length++;
@@ -46,10 +61,12 @@ class linkedList{
       return 1;
     }
     let node = new Node(value);
-    let prev = this._toIndex(index-1);
-    let nxt = prev.next;
+    let prv = this._toIndex(index-1);
+    let nxt = prv.next;
+    node.prev = prv
     node.next = nxt;
-    prev.next = node;
+    prv.next = node;
+    nxt.prev = node;
     this.length++;
   }
   _toIndex(index){
@@ -71,22 +88,24 @@ class linkedList{
     return index;
   }
   delete(value){
-    
     let index = this._detect(value);
-    let prev = this._toIndex(index-1);
-    let node = prev.next;
-    prev.next = node.next
+    let prv = this._toIndex(index-1);
+    let node = prv.next;
+    let nxt = node.next;
+    prv.next = node.next
+    nxt.prev = prv;
     node.next = null;
+    node.prev = null;
     this.length--;
   }
 }
 
 let list = new linkedList(50);
+list.prepend(0);
 list.append(60);
 list.append(70);
 list.append(80);
-list.insert(45,3);
-list.insert(44,5);
+list.insert(1,1);
 list.show();
-// list.delete(45);
-// list.show();
+list.delete(1);
+list.showReverse();
